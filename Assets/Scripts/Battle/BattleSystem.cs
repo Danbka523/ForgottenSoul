303ffad7b294b787bcf87c1df5ef66107a564696
Danbka523 <gordonanet12@gmail.com>
2021-12-01 15:37:58 +0300

    модифицирована пошаговая боевая система

--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -17,47 +17,220 @@ public class BattleSystem : MonoBehaviour
 
     //Units
     Unit playerUnit;
-    Unit EnemyUnit;
+    Unit enemyUnit;
 
     //BattleState
     public BattleState state;
+    public int roundCounter;
+
 
     void Start()
     {
-        state = BattleState.PLAYERTURN;
+        state = BattleState.START;
         playerUnit = playerPrefab.GetComponent<Unit>();
-        EnemyUnit = enemyPrefab.GetComponent<Unit>();
+        enemyUnit = enemyPrefab.GetComponent<Unit>();
+        BattleStart();
     }
 
 
-    public void PlayerAttack() {
-        if (state != BattleState.PLAYERTURN)
-            return;
-        EnemyUnit.DealPhysDamage(playerUnit.damage);
-        StartCoroutine(ChangeDialog("damage","player","enemy",playerUnit.damage));
-        if (EnemyUnit.isDead)
+    private void BattleStart()
+    {
+        roundCounter = 0;
+        state = BattleState.PLAYERTURN;
+        UnitTurn();
+    }
+
+    public void UnitTurn()
+    {
+        CheckFire();
+        CheckFreeze();
+        if (state == BattleState.PLAYERTURN)
+            roundCounter++;
+        if (playerUnit.currentHp == 0)
+        {
+            state = BattleState.LOSE;
+            EndBattle();
+        }
+        if (enemyUnit.currentHp == 0)
         {
             state = BattleState.WIN;
-            ChangeDialog(state);
+            EndBattle();
         }
-       // else
-           // state = BattleState.ENEMYTURN;
-
+        if (state == BattleState.ENEMYTURN)
+            StartCoroutine(EnemyTurn());
     }
 
+    //������� �����
+    public void PhysAttack()
+    {
+        Actions action = Actions.DAMAGE;
+        if (state == BattleState.PLAYERTURN)
+        {
+            enemyUnit.DealPhysDamage(playerUnit.damage);
+            StartCoroutine(ChangeDialog(action, playerUnit.name, enemyUnit.name, playerUnit.damage));
+            state = BattleState.ENEMYTURN;
+            UnitTurn();
+        }
+        else if (state == BattleState.ENEMYTURN)
+        {
+            playerUnit.DealPhysDamage(enemyUnit.damage);
+            StartCoroutine(ChangeDialog(action, enemyUnit.name, playerUnit.name, enemyUnit.damage));
+            state = BattleState.PLAYERTURN;
+            UnitTurn();
+        }
+        else
+            return;
+    }
+    //�������� ����� (wip)
+    //fire
+    //========================//
+    public void FireAttack()
+    {
+        Actions action = Actions.DAMAGE;
+        if (state == BattleState.PLAYERTURN)
+        {
+            enemyUnit.DealFireDamage(playerUnit.damage, roundCounter);
+            StartCoroutine(ChangeDialog(action, playerUnit.name, enemyUnit.name, playerUnit.damage));
+            state = BattleState.ENEMYTURN;
+            UnitTurn();
+        }
+        else if (state == BattleState.ENEMYTURN)
+        {
+            playerUnit.DealFireDamage(enemyUnit.damage, roundCounter);
+            StartCoroutine(ChangeDialog(action, enemyUnit.name, playerUnit.name, playerUnit.damage));
+            state = BattleState.PLAYERTURN;
+            UnitTurn();
+        }
+        else
+            return;
+    }
 
-   public void EnemyTurn() { }
+    private void CheckFire()
+    {
+        //�������� ������
+        if (playerUnit.isFired)
+        {
+            playerUnit.DealDamage(8);
+            ChangeDialog(Actions.FIRE, "", playerUnit.name);
+            if (roundCounter - playerUnit.negativeEffectRound == 3)
+            {
+                playerUnit.isFired = false;
+                playerUnit.isNegatived = false;
+            }
+        }
+        //�������� ����������
+        if (enemyUnit.isFired)
+        {
+            ChangeDialog(Actions.FIRE, "", enemyUnit.name);
+            enemyUnit.DealDamage(8);
+            if (roundCounter - enemyUnit.negativeEffectRound == 3)
+            {
+                enemyUnit.isFired = false;
+                enemyUnit.isNegatived = false;
+            }
+        }
+    }
+    //fire
+    //========================//
 
 
-    IEnumerator ChangeDialog(string action, string from, string to, int hp) {
-        string str = $"{from} damaged {to} on {hp} hp\n";
-        if (action == "damage")
-            historyDialog.text += str;
-        yield return new WaitForSeconds(5f);
+    //������� �����(wip)
+    //ice
+    //========================//
+    public void IceAttack()
+    {
+        Actions action = Actions.DAMAGE;
+        if (state == BattleState.PLAYERTURN)
+        {
+            enemyUnit.DealIceDamage(playerUnit.damage, roundCounter);
+            StartCoroutine(ChangeDialog(action, playerUnit.name, enemyUnit.name, playerUnit.damage));
+            state = BattleState.ENEMYTURN;
+            UnitTurn();
+        }
+        else if (state == BattleState.ENEMYTURN)
+        {
+            playerUnit.DealIceDamage(enemyUnit.damage, roundCounter);
+            StartCoroutine(ChangeDialog(action, enemyUnit.name, playerUnit.name, playerUnit.damage));
+            state = BattleState.PLAYERTURN;
+            UnitTurn();
+        }
+        else
+            return;
+    }
+
+    private void CheckFreeze()
+    {
+        //�������� ������
+        if (playerUnit.isFreezed && state == BattleState.PLAYERTURN)
+        {
+            state = BattleState.ENEMYTURN;
+            if (roundCounter - playerUnit.negativeEffectRound == 3)
+            {
+                playerUnit.isFreezed = false;
+                playerUnit.isNegatived = false;
+            }
+        }
+        //�������� ����������
+        if (enemyUnit.isFreezed && state == BattleState.ENEMYTURN)
+        {
+            state = BattleState.PLAYERTURN;
+            if (roundCounter - enemyUnit.negativeEffectRound == 3)
+            {
+                enemyUnit.isFreezed = false;
+                enemyUnit.isNegatived = false;
+            }
+        }
+    }
+
+    //ice
+    //========================//
+
+
+
+    //��� ���������� (wip)
+    //enemy
+    //========================//
+    IEnumerator EnemyTurn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        PhysAttack();
+    }
+
+    //enemy
+    //========================//
+
+
+    private void EndBattle()
+    {
+        ChangeDialog();
+
+    }
+    IEnumerator ChangeDialog(Actions action, string from = "", string to = "", int hp = 0)
+    {
+        string str = "";
+        switch (action)
+        {
+            case Actions.DAMAGE:
+                str = $"{from} damaged {to} on {hp} hp\n";
+                break;
+            case Actions.FIRE:
+                str = $"{from} is on fire\n";
+                break;
+            case Actions.FREEZE:
+                str = $"{from} is freese\n";
+                break;
+            case Actions.HEAL:
+                str = $"{from} healed {to} on {hp} hp\n";
+                break;
+            default:
+                break;
+        }
+        historyDialog.text += str;
+        yield return new WaitForSeconds(3.5f);
         historyDialog.text = "Battle log:\n";
     }
 
-    void ChangeDialog(BattleState state )
+    private void ChangeDialog()
     {
         if (state == BattleState.WIN)
             historyDialog.text = "CONGRATULATIONS YOU WIN!";
--- Assets/Scripts/Battle/Unit.cs
+++ Assets/Scripts/Battle/Unit.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+
     public string unitName;
     public int unitLVL;
 
@@ -12,16 +13,68 @@ public class Unit : MonoBehaviour
     public int maxHp;
     public int currentHp;
     public int defense;
+    //private int maxDefense = 30;
 
     public bool isParalysed;
-    public bool isSnowed;
+    public bool isFreezed;
     public bool isFired;
+    public bool isNegatived;
+   
+
     public bool isDead;
-    public void DealPhysDamage(int amount) {
-        currentHp -= amount;
-        if (currentHp <= 0) {
+
+    public int negativeEffectRound;
+
+    //обынчый урон
+    public void DealPhysDamage(int amount, int percentBoost=0) {
+        int currentDMG = amount + amount * percentBoost / 100;
+        currentHp -= (currentDMG - currentDMG * defense / 100);
+        if (currentHp < 0) {
             currentHp = 0;
             isDead = true;
         }
     }
+
+    //огненный урон
+    public void DealFireDamage(int amount,int curRound) {
+        DealPhysDamage(amount);
+        if (Random.Range(1, 101) >= 92 && !isNegatived)
+        {
+            isFired = true;
+            isNegatived = true;
+            negativeEffectRound = curRound;
+        }
+    }
+
+    //ледяной урон
+    public void DealIceDamage(int amount, int curRound)
+    {
+        DealPhysDamage(amount);
+        if (Random.Range(1, 101) >= 92 && !isNegatived)
+        {
+            isFreezed = true;
+            isNegatived = true;
+            negativeEffectRound = curRound;
+        }
+    }
+
+    //урон который игнорирует показатель брони
+    public void DealDamage(int amout)
+    {
+        currentHp -= amout;
+        if (currentHp < 0)
+        {
+            currentHp = 0;
+            isDead = true;
+        }
+    }
+
+    //дечение
+    public void HealUnit(int amout, int percetBoost = 0) {
+        currentHp += amout+amout*percetBoost/100;
+        if (currentHp > maxHp)
+            currentHp = maxHp;
+    }
+
+
 }
