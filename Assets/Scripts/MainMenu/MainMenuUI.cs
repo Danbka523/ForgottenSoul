40439d8b48118336e6093b6ddb0893a2592e5abe
Danbka523 <gordonanet12@gmail.com>
2021-12-05 12:57:07 +0300

    обновлено главное меню

--- Assets/Scripts/MainMenu/MainMenuUI.cs
+++ Assets/Scripts/MainMenu/MainMenuUI.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenuUI : MonoBehaviour
 {
+    //dropdown
+    public TMP_Dropdown resolutionDropdown;
+    private List<string> resolutions;
+
+    //audio
+    public AudioMixer audioMixer;
+
+    //slider
+    public Slider volumeSlider;
+
+
+    //ui
+    public GameObject mainMenu;
+    public GameObject settings;
+    private void Start()
+    {
+        int curr=0;
+        resolutions = new List<string>();
+        for (int i=0;i<Screen.resolutions.Length;i++)
+        {
+            string resStr = $"{Screen.resolutions[i].width}x{Screen.resolutions[i].height}x{Screen.resolutions[i].refreshRate}";
+            resolutions.Add(resStr);
+            if (Screen.resolutions[i].Equals(Screen.currentResolution))
+                curr = i;
+        }
+        resolutionDropdown.AddOptions(resolutions);
+        resolutionDropdown.value = curr;
+    }
+
     public void ExitGame() {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.ExitPlaymode();
@@ -17,5 +47,29 @@ public class MainMenuUI : MonoBehaviour
 
     public void StartGame() {
         SceneManager.LoadScene(1);
+    }
+    public void ResolutionChange()
+    {
+        int curr = resolutionDropdown.value;
+        Screen.SetResolution(Screen.resolutions[curr].width,Screen.resolutions[curr].height,true,Screen.resolutions[curr].refreshRate);
+    }
+
+    public void VolumeChange()
+    {
+        if (volumeSlider.value == 0)
+            audioMixer.SetFloat("Volume",-80f);
+        else
+            audioMixer.SetFloat("Volume", Mathf.Log10(volumeSlider.value)*30);
+    }
+
+    public void OnSettingsClick() {
+        mainMenu.SetActive(false);
+        settings.SetActive(true);
+      
+    }
+
+    public void OnBackClick() {
+        settings.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
