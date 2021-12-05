68472531d4919b9ef859097a918cf185b926eb0c
Danbka523 <gordonanet12@gmail.com>
2021-12-05 12:56:53 +0300

    шутерная часть(Кирилл)

---
+++ Assets/Scripts/Player/CreateBullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBullet : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab_bullet;
+    [SerializeField] private AudioSource audio;
+    [SerializeField] ParticleSystem particle;
+
+    private void Update()
+    {
+        Shoot();
+    }
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newbullet = Instantiate(prefab_bullet, new Vector3(transform.position.x,transform.position.y+0.05f,transform.position.z), transform.rotation);
+
+            audio.Play();
+            particle.Play();
+        }
+    }
+
+}
