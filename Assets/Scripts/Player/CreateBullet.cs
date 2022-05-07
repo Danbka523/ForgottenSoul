85a3385925ab94d7dc2c4e048c3102278dd90b82
Danbka523 <gordonanet12@gmail.com>
2022-05-07 20:03:21 +0300

    +добавлена перезарядка

--- Assets/Scripts/Player/CreateBullet.cs
+++ Assets/Scripts/Player/CreateBullet.cs
@@ -1,15 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateBullet : MonoBehaviour
 {
     [SerializeField] private GameObject prefab_bullet;
     [SerializeField] private AudioSource audio;
+
+    [SerializeField] AudioClip shoot;
+    [SerializeField] AudioClip empty;
+    [SerializeField] AudioClip reload_sound;
+
     [SerializeField] ParticleSystem particle;
     [SerializeField] PlayerMovement player;
     private LineRenderer laser;
-
+    private int ammo;
+    private bool isReloading;
     private void Awake()
     {
+        ammo = 13;
         laser = GetComponent<LineRenderer>();
     }
 
@@ -22,13 +31,34 @@ public class CreateBullet : MonoBehaviour
     {
         if (!player.isPaused)
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                GameObject newbullet = Instantiate(prefab_bullet, new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z), transform.rotation);
-
-                audio.Play();
-                particle.Play();
+            if (Input.GetKeyDown(KeyCode.R)) {
+                StartCoroutine("reload");
             }
+            if (Input.GetMouseButtonDown(0) && !isReloading)
+            {
+                if (ammo > 0)
+                {
+                    GameObject newbullet = Instantiate(prefab_bullet, new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z), transform.rotation);
+
+                    audio.PlayOneShot(shoot);
+                    particle.Play();
+                    ammo--;
+                }
+                else {
+                    audio.PlayOneShot(empty);
+                }
+            }
+        }
+    }
+
+    IEnumerator reload() {
+        if (!isReloading)
+        {
+            isReloading = true;
+            audio.PlayOneShot(reload_sound);
+            yield return new WaitForSeconds(0.6f);
+            ammo = 13;
+            isReloading = false;
         }
     }
 
